f4673537560bd91840440d446deea7ca241abbe1
Thomasomoke <omokethomas644@gmail.com>
2024-11-17 16:10:01 +0300

    0-basic_cache.py

--- 0x01-caching/0-basic_cache.py
+++ 0x01-caching/0-basic_cache.py
@@ -3,23 +3,18 @@
 BaseCaching = __import__('base_caching').BaseCaching
 
 """
-This module implements a basic caching system that allows storing and
-retrieving items without any limit on the number of items stored.
+This module implements a basic caching system that allows storing items
 """
 
 
 class BasicCache(BaseCaching):
     """
     BasicCache is a caching system that inherits from BaseCaching.
-    It provides methods to add and retrieve items from the cache
-    without any restrictions on size.
     """
 
     def put(self, key, item):
         """
         Add an item to the cache_data dictionary.
-        This method stores the item associated with the given key if
-        both key and item are not None.
         """
         if key is not None and item is not None:
             self.cache_data[key] = item
@@ -27,8 +22,5 @@ class BasicCache(BaseCaching):
     def get(self, key):
         """
         Retrieve an item from cache_data by key.
-        This method returns the item associated with the given key if
-        it exists, or None if the key is None or does not exist in
-        the cache.
         """
         return self.cache_data.get(key) if key else None
